eeca8fce2b77a41658c0ad738740c99db2414b24
Ujjwal Kumar <ujjwal2282@gmail.com>
2026-07-03 06:31:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (34.98%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,33 @@
+class Solution {
+    int MOD = 1000000007;
+    public int countGoodNumbers(long n) {
+
+        // if(n==1){
+        //     return 5;
+        // }
+
+        // if(n%2==0){
+        //     return 4*countGoodNumbers(n-1)%MOD;
+        // }else{
+        //     return 5*countGoodNumbers(n-1)%MOD;
+        // }
+        long result=(power(5,(n-n/2))*power(4,n/2))%MOD;
+        return (int) result;
+    }
+
+    private long power(long x,long n){
+        if(n==0){
+            return 1;
+        }
+
+
+        long halfpow=power(x,(n/2))% MOD;
+        long sqpow=(halfpow*halfpow)% MOD;
+
+        if(n%2==0){
+            return sqpow;
+        }else{
+            return (x*sqpow)% MOD;
+        }
+    }
+}
